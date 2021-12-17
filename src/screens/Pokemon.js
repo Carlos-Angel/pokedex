c38b67a66390f49a1efffed698f9c1e9b1b30f51
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-16 21:38:29 -0600

    feat: cambiar la barra de navegación del screen pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { ScrollView, Text } from 'react-native';
+import { ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { getPokemonDetailsByIdApi } from '../api/pokemon';
 import Header from '../components/Pokemon/Header';
 import Stats from '../components/Pokemon/Stats';
@@ -8,6 +9,22 @@ import Type from '../components/Pokemon/Type';
 export default function Pokemon({ route: { params }, navigation }) {
   const [pokemon, setPokemon] = useState(null);
   const { id } = params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null, //TODO: add favorites
+      headerLeft: () => (
+        <Icon
+          name='arrow-left'
+          color='#fff'
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
+
   useEffect(() => {
     if (id) {
       (async () => {
