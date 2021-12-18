d50ac953416c70b685832cd656e331a624874796
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-18 08:17:48 -0600

    style: mock de usuario

---
+++ src/utils/userDB.js
@@ -0,0 +1,11 @@
+export const user = {
+  username: 'poke-user',
+  password: 'password',
+};
+
+export const userDetails = {
+  username: 'poke-user',
+  firstName: 'poke-first.name',
+  lastName: 'poke-last-name',
+  email: 'pokemon.api.com',
+};
