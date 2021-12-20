93b4332d85d011b363bca92c3fdd3e870e0f84d1
Charly_Angel <c.a.a.a.503@gmail.com>
2021-12-20 11:49:06 -0600

    feat: mostrar mis pokemons favoritos

--- src/screens/Favorites.js
+++ src/screens/Favorites.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState, useCallback } from 'react';
+import { Text, View } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { useAuth } from '../hooks/useAuth';
+import { getPokemonDetailsByIdApi } from '../api/pokemon';
+import { getPokemonsFavoriteApi } from '../api/favorite';
+import PokemonsList from '../components/PokemonsList';
 
 export default function Favorites() {
+  const { auth } = useAuth();
+  const [pokemons, setPokemons] = useState([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const results = await getPokemonsFavoriteApi();
+          const pokemonsDetails = [];
+
+          for await (const pokemonId of results) {
+            const { id, name, types, order, sprites } =
+              await getPokemonDetailsByIdApi(pokemonId);
+            const item = {
+              id,
+              name,
+              type: types[0].type.name,
+              order,
+              image: sprites.other['official-artwork'].front_default,
+            };
+            pokemonsDetails.push(item);
+          }
+
+          setPokemons([...pokemonsDetails]);
+        })();
+      }
+    }, [auth]),
+  );
   return (
-    <SafeAreaView>
-      <Text>Favorites</Text>
-    </SafeAreaView>
+    <View>
+      {!auth ? (
+        <Text>user no logged</Text>
+      ) : (
+        <PokemonsList pokemons={pokemons} />
+      )}
+    </View>
   );
 }
